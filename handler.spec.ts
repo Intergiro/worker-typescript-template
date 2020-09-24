a41f395e651805be38a79c378073c8fb3018ba71
Simon Mika <simon@mika.se>
2020-09-24 11:29:25 +0200

    Working template?

--- handler.spec.ts
+++ handler.spec.ts
@@ -1,5 +1,5 @@
 // set up global namespace for worker environment
-import * as makeServiceWorkerEnv from "service-worker-mock"
+import { default as makeServiceWorkerEnv } from "service-worker-mock"
 declare let global: any
 Object.assign(global, makeServiceWorkerEnv())
 
